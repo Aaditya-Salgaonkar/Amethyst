a73b87151623fc48fece44715debdd077aecec92
yash01491625 <yyashmalviya051@gmail.com>
2025-02-20 09:06:01 +0530

    Google Oauth

--- auth.js
+++ auth.js
@@ -1,26 +1,33 @@
-import passport from 'passport';
+import passport from "passport";
 import dotenv from "dotenv";
+import { Strategy as GoogleStrategy } from "passport-google-oauth2";
+
 dotenv.config();
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-import something from 'passport-google-oauth2';
-const GoogleStrategy = something.Strategy;
 passport.use(new GoogleStrategy({
-    clientID:     GOOGLE_CLIENT_ID,
+    clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:5002/google/callback",
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return done(err, user);
-    // });
-    return done(null, user);
-  }
-));
+    passReqToCallback: true
+}, 
 
-passport.serializeUser(function(user, done) {
+function(request, accessToken, refreshToken, profile, done) {
+    const user = {
+        id: profile.id,
+        displayName: profile.displayName,
+        email: profile.emails[0].value,
+        photo: profile.picture
+    };
+    return done(null, user);
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
     done(null, user);
 });
--- index.js
+++ index.js
@@ -3,63 +3,60 @@ import path from "path";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-// import { signIn, signUp } from "./auth/auth1.js";
 import dotenv from "dotenv";
-// import authRoutes from "./auth/auth2.js";
 import "./auth.js";
 import passport from "passport";
 import session from "express-session";
 
-function logedIn(req, res, next) {
-    if (req.user) {
-        next(); 
-    } else {
-        res.sendStatus(404);
-    }
-}
-
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(session({
-    secret: "abcd",
+    secret: process.env.SESSION_SECRET || "abcd",
     resave: false,       
     saveUninitialized: false 
 }));
-  
+
 app.use(passport.initialize());
 app.use(passport.session());
-const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, "frontend")));
 
-// app.use("/auth", authRoutes);
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next(); 
+    } else {
+        res.sendStatus(401); 
+    }
+}
+
 app.get("/", (req, res) => {
-    // res.sendFile(path.join(__dirname, "frontend", "login.html"));
-    res.send('<a href="/auth/google">SignIn with Google</a>');
+    res.send('<a href="/auth/google">Sign in with Google</a>');
 });
 
-app.get("/protected", logedIn, (req, res) => {
-    res.send("Hello!")
+app.get("/protected", loggedIn, (req, res) => {
+    res.send(`Hello, ${req.user.displayName || "User"}!`);
 });
 
 app.get("/auth/google", 
-    passport.authenticate('google', { scope: ['email', 'profile']})
-)
+    passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
 app.get("/google/callback", 
-    passport.authenticate('google', {
-        successRedirect: '/protected',
-        failureRedirect: '/auth/failure'
+    passport.authenticate("google", {
+        successRedirect: "/protected",
+        failureRedirect: "/auth/failure"
     })
-)
+);
 
-app.get('/auth/failure', (req, res) => {
-    res.send("Something went wrong");
-})
+app.get("/auth/failure", (req, res) => {
+    res.send("Authentication failed. Please try again.");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
